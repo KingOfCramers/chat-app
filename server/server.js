f2b61cee8e0fcbb1664f3f4e1ba1f8991328bccf
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-12-25 12:28:48 -0500

    Added io connect/disconnect

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
-const path = require("path");
-const express = require("express");
-const publicPath = path.join(__dirname, "..", "public");
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-const app = express();
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server); // Gives you access to a route that accepts incoming connections...
+// This is available at */socket.io/socket.io.js
 
-app.use("/", express.static(path.join(publicPath)));
-app.listen(port, (err) => {
-  if(err) throw err;
-  console.log(`Listening on port ${port}.`)
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected.');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  })
+//  console.log(socket)
+//   socket.on('disconnect', () => {
+//     console.log('User was disconnected');
+//   });
+ });
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
